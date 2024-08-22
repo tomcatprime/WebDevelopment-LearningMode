4234f75fe8d9817c47f7dc62fa5969bcb10e0403
tomcatprime <karolchru@gmail.com>
2024-08-22 10:42:04 +0300

    JS math.random

---
+++ JavaScript/Love Calculator/app.js
@@ -0,0 +1,16 @@
+// // random number
+// var n = Math.random();
+
+// n = n * 6;
+
+// // random number between 1 and 6
+// n = Math.floor(n * 6) + 1;
+
+
+var name1 = prompt("What is name no.1")
+var name2 = prompt("What is name no.2")
+
+var n = Math.random(n);
+n = n * 100;
+var loveScore = Math.floor(n) + "%";
+alert("Your love score is " + loveScore)
